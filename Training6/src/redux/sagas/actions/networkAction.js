7cbfe4894fe8213435393719104487e783915960
Thuc Le <thuclt@zigvy.com>
2021-03-09 00:09:50 +0700

    [thuclt] remove redundant action files

--- Training6/src/redux/sagas/actions/networkAction.js
+++
@@ -1,3 +0,0 @@
-import {createAction} from '@reduxjs/toolkit';
-
-export const saga_changeStatusNetworkAction = createAction('NETWORK_REQUEST_CHANGE_STATUS');
--- Training6/src/redux/sagas/actions/taskesAction.js
+++
@@ -1,4 +0,0 @@
-import {createAction} from '@reduxjs/toolkit';
-
-export const saga_submitTaskAction = createAction('TASK_REQUEST_SUBMIT');
-export const saga_changeStatusTaskAction = createAction('TASK_REQUEST_CHANGE_STATUS');
